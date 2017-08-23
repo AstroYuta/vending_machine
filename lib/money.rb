5c2c240f47bfbf898469e99f50d3b08fd9da7b99
Yuta Shimizu <yuta@Yutas-MacBook-Air.local>
2017-08-23 17:19:50 +0900

    price->amountに変更&VOになっているかチェックするテストを追加しました

--- lib/money.rb
+++ lib/money.rb
@@ -1,6 +1,10 @@
 class Money
-  attr_reader :price
-  def initialize(price:)
-    @price = price
+  attr_reader :amount
+  def initialize(amount:)
+    @amount = amount
+  end
+
+  def ==(other_money)
+    self.amount == other_money.amount
   end
 end
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,19 +1,34 @@
 require "spec_helper"
 
 RSpec.describe Money do
-  let(:money) {Money.new(price: price)}
+  let(:money) {Money.new(amount: amount)}
 
-  describe '#money' do
-    subject { money.price }
+  describe '#money.amount' do
+    subject { money.amount }
 
     context '金額が100円のとき' do
-      let(:price) { 100 }
+      let(:amount) { 100 }
       it { is_expected.to eq 100 }
     end
 
     context '金額が500円のとき' do
-      let(:price) { 500 }
+      let(:amount) { 500 }
       it { is_expected.to eq 500 }
     end
   end
+
+  describe '#money' do
+    subject {money}
+
+    context '全く同じインスタンスが作られたとき' do
+      let(:amount) { 100 }
+      it { is_expected.to eq Money.new(amount: 100) }
+    end
+
+    context '違うインスタンスが作られた時' do
+      let(:amount) { 100 }
+      it { is_expected.not_to eq Money.new(amount: 150)}
+    end
+  end
+
 end
