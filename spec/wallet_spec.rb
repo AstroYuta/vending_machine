af0d854666d41bb24a344861fca11584f1cbfa6b
Yuta Shimizu <yuta@Yutas-MacBook-Air.local>
2017-08-24 11:38:27 +0900

    Money classが所持金額がマイナスの時エラーを返すように実装

--- spec/wallet_spec.rb
+++ spec/wallet_spec.rb
@@ -4,24 +4,30 @@ require "wallet"
 RSpec.describe Wallet do
   let(:wallet) { Wallet.new(sum_of_money: sum_of_money) }
   describe '#sum_of_money' do
-    
+    #所持金額が実装されているかテストする   
     subject {wallet.sum_of_money}
 
     shared_examples 'sum_of_money' do |x|
       context "所持金額が#{x}円のとき" do
         let(:sum_of_money) { x }
-        it { is_expected.to eq x }
+        it { is_expected.to eq x}
       end
     end
 
-    include_examples 'sum_of_money', 1000
+    include_examples 'sum_of_money', 0
     include_examples 'sum_of_money', 500
     include_examples 'sum_of_money', 1234
     include_examples 'sum_of_money', 32450
     include_examples 'sum_of_money', 50000
-
-
   end
+
+    context '所持金額がマイナスのとき' do
+      it {
+        expect { Wallet.new(sum_of_money: -100) }.to raise_error(ArgumentError)
+      }
+    end
+
+
 
 
 end
