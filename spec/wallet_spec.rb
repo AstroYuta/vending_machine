661bb9758ede90e3bfdb11d9108812911af33e9a
Yuta Shimizu <yuta@Yutas-MacBook-Air.local>
2017-08-24 10:31:46 +0900

    money classが所持金額を持つように実装

--- spec/wallet_spec.rb
+++ spec/wallet_spec.rb
@@ -1,7 +1,27 @@
 require "spec_helper"
+require "wallet"
 
 RSpec.describe Wallet do
-  let(:wallet) { Wallet.new(sum_of_money: sum_of_money)}
+  let(:wallet) { Wallet.new(sum_of_money: sum_of_money) }
+  describe '#sum_of_money' do
+    
+    subject {wallet.sum_of_money}
+
+    shared_examples 'sum_of_money' do |x|
+      context "所持金額が#{x}円のとき" do
+        let(:sum_of_money) { x }
+        it { is_expected.to eq x }
+      end
+    end
+
+    include_examples 'sum_of_money', 1000
+    include_examples 'sum_of_money', 500
+    include_examples 'sum_of_money', 1234
+    include_examples 'sum_of_money', 32450
+    include_examples 'sum_of_money', 50000
+
+
+  end
 
 
 end
