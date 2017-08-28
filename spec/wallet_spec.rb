e6485965b49c60dcf624d0317c09751d7eb3e05b
yagince <straitwalk@gmail.com>
2017-08-28 12:14:50 +0900

    メソッド毎にspecを書く為にtake_outのspecを追加

--- spec/wallet_spec.rb
+++ spec/wallet_spec.rb
@@ -26,10 +26,6 @@ RSpec.describe Wallet do
       it { is_expected.to eq 500 }
     end
 
-    context '初めに1000円入っていて、そこから5000円取り出すとき' do
-      it { expect { wallet.take_out(Money.new(amount: 5000)) }.to raise_error(ArgumentError) }
-    end
-
     context '1000円入っている財布と1000入っている財布を統合したとき' do
       it { expect(wallet.merge(Wallet.new(money: Money.new(amount: 1000))).sum_of_money).to eq 2000 }
     end
@@ -52,6 +48,29 @@ RSpec.describe Wallet do
 
     context '-100円足した場合' do
       let(:add_money_amount) { -100 }
+      it {
+        expect { subject }.to raise_error ArgumentError
+      }
+    end
+  end
+
+  describe '#take_out' do
+    let(:default_money_amount) { 1000 }
+
+    subject { wallet.take_out(Money.new(amount: take_out_money_amount)) }
+
+    context '100円引いた場合' do
+      let(:take_out_money_amount) { 100 }
+      it { expect(subject.money.amount).to eq 900 }
+    end
+
+    context '900円引いた場合' do
+      let(:take_out_money_amount) { 900 }
+      it { expect(subject.money.amount).to eq 100 }
+    end
+
+    context '1100円引いた場合' do
+      let(:take_out_money_amount) { 1100 }
       it {
         expect { subject }.to raise_error ArgumentError
       }
